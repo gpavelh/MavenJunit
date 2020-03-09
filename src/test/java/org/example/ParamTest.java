8af7f261da6af49547efd2de34fade4e1c5658ec
gh <gh7777@mail.ru>
2020-03-09 10:03:56 +0300

    v3.0

--- src/test/java/org/example/ParamTest.java
+++ src/test/java/org/example/ParamTest.java
@@ -2,26 +2,26 @@ package org.example;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.stream.Stream;
+
 
 public class ParamTest extends Assertions {
     private static final Logger LOG = LoggerFactory.getLogger(ParamTest.class);
 
-    static Stream<Arguments> localParameters() {
-        return Stream.of(
-                Arguments.of("A", "B", "AB"),
-                Arguments.of("C", "D", "CD"),
-                Arguments.of("X", "C", "XC"));
+    private static Object[][] dataProvider() {
+        return new Object[][]{
+                {"A", "B", "AB"},
+                {"C", "D", "CD"},
+                {"X", "C", "XC"}
+        };
     }
 
     @DisplayName("ParamTest")
     @ParameterizedTest
-    @MethodSource("localParameters")
+    @MethodSource("dataProvider")
     public void paramTest1(String valueA, String valueB, String result) {
         assertEquals(result, valueA + valueB);
         LOG.info("All good");
